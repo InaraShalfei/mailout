22ae27fa8e00112d7177cdf6878d0d9a4c5b4dbc
InaraShalfei <inarashugaikina@gmail.com>
2022-03-01 11:09:48 +0500

    Install DRF module

--- mailout/settings.py
+++ mailout/settings.py
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
