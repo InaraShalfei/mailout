22340726113c58513516d6e37caf76af852cd43e
InaraShalfei <inarashugaikina@gmail.com>
2022-03-01 13:15:47 +0500

    Register models in admin zone

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import MailOut, Client, Message
+
+
+class MailOutAdmin(admin.ModelAdmin):
+    list_filter = ('start_time', 'finish_time',)
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_filter = ('tag', 'operator_code', 'timezone',)
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_filter = ('status',)
+
+
+admin.site.register(MailOut, MailOutAdmin)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Message, MessageAdmin)
