37ff07f0a0768c26fd308cb62a94e52dcad949a0
InaraShalfei <inarashugaikina@gmail.com>
2022-03-02 17:58:08 +0600

    Fix functionality for sending messages with external API

--- api/services.py
+++ api/services.py
@@ -16,27 +16,31 @@ def process_planned_message(pk):
         message.save()
         return
     send_message = SendMessage()
-    send_message.send_message(message.client.phone_number, message.mailout.text)
-    message.status = 'is_sent'
+    successful = send_message.send_message(message)
+    if successful:
+        message.status = 'is_sent'
+    else:
+        message.status = 'error'
     message.save()
 
 
 class SendMessage:
     def send_message(self, message):
-        data = {
-            "id": message.id,
-            "phone": int(message.client.phone_number),
-            "text": message.mailout.text
-        }
-        response = requests.post(
-            f'https://probe.fbrq.cloud/v1/send/{message.id}',
-            data=json.dumps(data),
-            headers={'Authorization': f'Bearer {os.getenv("TOKEN")}'})
-
-# парсить response.text ({"code":0,"message":"OK"}) и если message=OK return true, else - return false
-        # try/except В except return False
-
-        return response.status_code
+        try:
+            data = {
+                'id': message.id,
+                'phone': int(message.client.phone_number),
+                'text': message.mailout.text
+            }
+            response = requests.post(
+                f'https://probe.fbrq.cloud/v1/send/{message.id}',
+                data=json.dumps(data),
+                headers={'Authorization': f'Bearer {os.getenv("TOKEN")}'})
+            if response.json().get('message') != 'OK':
+                return False
+            return True
+        except:
+            return False
 
 
 class MailoutService:
