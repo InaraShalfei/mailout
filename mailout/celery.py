7ecc61c3b9d736b60b4c0e3566b0c5fdc1739eba
InaraShalfei <inarashugaikina@gmail.com>
2022-03-03 15:19:48 +0600

    Add rabbitmq container

--- mailout/celery.py
+++ mailout/celery.py
@@ -1,7 +1,14 @@
-from celery import Celery
+import os
 
+from celery import Celery
+from dotenv import load_dotenv
+
+load_dotenv()
+
+user = os.getenv('RABBITMQ_USERNAME')
+password = os.getenv('RABBITMQ_PASSWORD')
 app = Celery('mailout',
-             broker='amqp://localhost',
+             broker=f'amqp://{user}:{password}@rabbitmq',
              include=['mailout.tasks'])
 
 if __name__ == '__main__':
