df0a695241706ea32372f690d291a14ecd9244d5
InaraShalfei <inarashugaikina@gmail.com>
2022-03-03 18:20:16 +0600

    Add server IP into allowed hosts

--- mailout/settings.py
+++ mailout/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web', '172.19.0.3']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web', '172.19.0.3', '85.143.172.31']
 
 
 # Application definition
