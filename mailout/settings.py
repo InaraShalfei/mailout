01c7d352b0fbec3e161f4010c0985b198b8fb321
InaraShalfei <inarashugaikina@gmail.com>
2022-03-04 16:52:16 +0600

    Create /docs/ url for openapi documentation

--- mailout/settings.py
+++ mailout/settings.py
@@ -8,7 +8,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -18,8 +17,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web', '172.19.0.3', '85.143.172.31']
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web', '172.19.0.3',
+                 '85.143.172.31']
 
 # Application definition
 
@@ -33,6 +32,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'django_filters',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +64,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mailout.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
@@ -98,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -110,13 +108,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SWAGGER_SETTINGS = {
+    'DEFAULT_API_URL': os.getenv('APP_URL'),
+}
--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 python manage.py makemigrations api
+python manage.py collectstatic --noinput
 python manage.py migrate api
 python manage.py migrate --noinput
 python manage.py runserver 0.0.0.0:8000
